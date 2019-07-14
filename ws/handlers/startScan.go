08b775ed49163455a15741183c688a4f3aa94d7d
hednowley <13064635+hednowley@users.noreply.github.com>
2019-07-14 18:55:59 +0100

    Scan delete

--- ws/handlers/startScan.go
+++ ws/handlers/startScan.go
@@ -11,12 +11,19 @@ import (
 func MakeStartScanHandler(scanner *provider.Scanner) ws.WsHandler {
 	return func(request *dto.Request) interface{} {
 		var update bool
+		var delete bool
+
 		err := json.Unmarshal(*request.Params["update"], &update)
 		if err != nil {
 
 		}
 
-		go scanner.StartAllScans(update, false)
+		err = json.Unmarshal(*request.Params["delete"], &delete)
+		if err != nil {
+
+		}
+
+		go scanner.StartAllScans(update, delete)
 		return struct{}{}
 	}
 }
