2a082ef04c2d5bcc129316a9fc134e975aa85748
hednowley <13064635+hednowley@users.noreply.github.com>
2019-08-11 15:40:07 +0100

    Check provider names are unique

--- provider/provider.go
+++ provider/provider.go
@@ -1,6 +1,8 @@
 package provider
 
 import (
+	"fmt"
+
 	"github.com/cihub/seelog"
 	"github.com/hednowley/sound/config"
 	"github.com/hednowley/sound/entities"
@@ -25,24 +27,43 @@ type Provider interface {
 	FileCount() int64
 }
 
+func addProvider(providers []Provider, provider Provider) ([]Provider, error) {
+	for _, p := range providers {
+		if p.ID() == provider.ID() {
+			return providers, fmt.Errorf("Duplicate provider name (%v)", provider.ID())
+		}
+	}
+	providers = append(providers, provider)
+	return providers, nil
+}
+
 func NewProviders(config *config.Config) []Provider {
 	ps := []Provider{}
 
 	for _, fs := range config.FileSystemProviders {
-		p, err := NewFsProvider("fs", fs.Path, fs.Extensions)
+
+		p, err := NewFsProvider(fs.Name, fs.Path, fs.Extensions)
 		if err != nil {
 			seelog.Errorf("Could not create file system provider: %v", err)
-		} else {
-			ps = append(ps, p)
+			continue
+		}
+
+		ps, err = addProvider(ps, p)
+		if err != nil {
+			seelog.Errorf("Could not create file system provider: %v", err)
 		}
 	}
 
 	for _, b := range config.BeetsProviders {
-		p, err := NewBeetsProvider("beets", b.Database)
+		p, err := NewBeetsProvider(b.Name, b.Database)
 		if err != nil {
 			seelog.Errorf("Could not create beets provider: %v", err)
-		} else {
-			ps = append(ps, p)
+			continue
+		}
+
+		ps, err = addProvider(ps, p)
+		if err != nil {
+			seelog.Errorf("Could not create beets provider: %v", err)
 		}
 	}
 
